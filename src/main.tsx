fb219fef778d97235f5cf51b773fc40cad8e6a82
Vicente Martinez Gomez <vicente.martinezgz@gmail.com>
2026-01-21 15:30:46 -0500

    adds lesson 162

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'sonner';
 import './index.css'
+import { ProfessionalApp } from './useContext/ProfessionalApp';
 //import { TrafficLight } from './useState/TrafficLight';
 //import { TrafficLightWithEffect } from './useEffect/TrafficLightWithEffect';
 //import { PokemonPage } from './examples/PokemonPage';
@@ -11,14 +12,14 @@ import './index.css'
 //import { MemoCounter } from './memos/MemoCounter';
 //import { ScrambleWords } from './useReducer/ScrambleWords';
 //import { InstagromApp } from './optimistic/InstagromApp';
-import { ClientInformation } from './use_suspense/ClientInformation';
-import { getUserAction } from './api/get-user.action';
+//import { ClientInformation } from './use_suspense/ClientInformation';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Toaster richColors/>
     <Suspense fallback={<h1>Cargando...</h1>}>
-      <ClientInformation getUser={ getUserAction(2333) } />
+      <ProfessionalApp />
     </Suspense>
     
   </StrictMode>,
---
+++ src/useContext/ProfessionalApp.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ProfessionalApp = () => {
+  return (
+    <div className ="bg-gradient flex flex-col">
+        <h1>Professional App</h1>
+    </div>
+  )
+}
